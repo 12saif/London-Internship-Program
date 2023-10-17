2822c7f4e67cbafa709060fd0f425476d49227c7
md saif <mohdsaif9170613062@gmail.com>
2023-10-17 16:43:03 +0530

    header in progress

--- src/Components/Header/TopHeader.jsx
+++ src/Components/Header/TopHeader.jsx
@@ -1,15 +1,41 @@
 import React from "react";
 import "./TopHeader.css";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SellOutlinedIcon from "@mui/icons-material/SellOutlined";
+import PersonAddAltOutlinedIcon from "@mui/icons-material/PersonAddAltOutlined";
+import PersonSearchOutlinedIcon from "@mui/icons-material/PersonSearchOutlined";
+import RecordVoiceOverOutlinedIcon from "@mui/icons-material/RecordVoiceOverOutlined";
+// import { red } from "@mui/material/colors";
 
 const TopHeader = () => {
   return (
     <header className="header_Conatainer">
       <div className="left-bar">
         London Internship Program
-        <span>London</span>
+        <span className="london">London</span>
       </div>
-      <div className="mid-bar">247 | 243 Opportunity Browsing</div>
-      <div className="right-bar">Move To Video Interview I</div>
+      <div className="mid-bar">
+        Opportunity Browsing
+        <KeyboardArrowDownIcon />
+      </div>
+      <div className="right-bar">
+        <div className="button-check_user">
+          <SellOutlinedIcon color="#444444" sx={{ fontSize: 22 }} />
+        </div>
+        <div className="button-check_user">
+          <PersonAddAltOutlinedIcon color="secondary" sx={{ fontSize: 22 }} />
+        </div>
+        <div className="button-check_user">
+          <PersonSearchOutlinedIcon color="#444444" sx={{ fontSize: 22 }} />
+        </div>
+        <div className="button-check_user">
+          <RecordVoiceOverOutlinedIcon color="#444444" sx={{ fontSize: 22 }} />
+        </div>
+        <div className="right_videoCall-button-container">
+          <div className="right_videoCall-button">Move To Video Interview</div>
+          <KeyboardArrowDownIcon />
+        </div>
+      </div>
     </header>
   );
 };
