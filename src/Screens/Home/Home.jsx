0360d9e4a512b3b5e69abc08f07f588c350f9a66
md saif <mohdsaif9170613062@gmail.com>
2023-10-18 23:17:30 +0530

    header Completed

---
+++ src/Screens/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+import TopHeader from "./Header/TopHeader";
+
+const Home = () => {
+  return (
+    <div className="home_container">
+      <TopHeader />
+    </div>
+  );
+};
+
+export default Home;
