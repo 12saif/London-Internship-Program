b2ffc89a6d80acdb45ee2ca40bb4cef49b78dd17
md saif <mohdsaif9170613062@gmail.com>
2023-10-18 23:28:12 +0530

    Create fees section

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./Home.css";
 import TopHeader from "./Header/TopHeader";
+import SrearchArea from "./SearchArea/SearchArea";
+import Feed from './Feed/Feed';
 
 const Home = () => {
   return (
     <div className="home_container">
-      <TopHeader />
+      <div className="home_header">
+        <TopHeader />
+      </div>
+      <div className="search-and-FeedAreas">
+        <SrearchArea />
+        <Feed />
+      </div>
     </div>
   );
 };
---
+++ src/Screens/Home/SearchArea/SearchArea.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SrearchArea = () => {
+  return (
+    <div>SrearchArea</div>
+  )
+}
+
+export default SrearchArea
